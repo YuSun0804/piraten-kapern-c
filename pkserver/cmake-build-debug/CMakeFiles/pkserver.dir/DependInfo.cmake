0eaa7d28fa5f2ed294e7c8d19a8db7b633995d62
YuSun <yusun@example.com>
2020-11-23 00:04:12 -0500

    upd

---
+++ pkserver/cmake-build-debug/CMakeFiles/pkserver.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/kevin/repo/c++/piraten-kapern-c/pkserver/src/ThreadPool.cpp" "/home/kevin/repo/c++/piraten-kapern-c/pkserver/cmake-build-debug/CMakeFiles/pkserver.dir/src/ThreadPool.cpp.o"
+  "/home/kevin/repo/c++/piraten-kapern-c/pkserver/src/Util.cpp" "/home/kevin/repo/c++/piraten-kapern-c/pkserver/cmake-build-debug/CMakeFiles/pkserver.dir/src/Util.cpp.o"
+  "/home/kevin/repo/c++/piraten-kapern-c/pkserver/src/httpparse.cpp" "/home/kevin/repo/c++/piraten-kapern-c/pkserver/cmake-build-debug/CMakeFiles/pkserver.dir/src/httpparse.cpp.o"
+  "/home/kevin/repo/c++/piraten-kapern-c/pkserver/src/httpresponse.cpp" "/home/kevin/repo/c++/piraten-kapern-c/pkserver/cmake-build-debug/CMakeFiles/pkserver.dir/src/httpresponse.cpp.o"
+  "/home/kevin/repo/c++/piraten-kapern-c/pkserver/src/main.cpp" "/home/kevin/repo/c++/piraten-kapern-c/pkserver/cmake-build-debug/CMakeFiles/pkserver.dir/src/main.cpp.o"
+  "/home/kevin/repo/c++/piraten-kapern-c/pkserver/src/server.cpp" "/home/kevin/repo/c++/piraten-kapern-c/pkserver/cmake-build-debug/CMakeFiles/pkserver.dir/src/server.cpp.o"
+  "/home/kevin/repo/c++/piraten-kapern-c/pkserver/src/ssocket.cpp" "/home/kevin/repo/c++/piraten-kapern-c/pkserver/cmake-build-debug/CMakeFiles/pkserver.dir/src/ssocket.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ pkserver/cmake-build-debug/CMakeFiles/pkserver.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pkserver.dir/src/ThreadPool.cpp.o"
+  "CMakeFiles/pkserver.dir/src/Util.cpp.o"
+  "CMakeFiles/pkserver.dir/src/httpparse.cpp.o"
+  "CMakeFiles/pkserver.dir/src/httpresponse.cpp.o"
+  "CMakeFiles/pkserver.dir/src/main.cpp.o"
+  "CMakeFiles/pkserver.dir/src/server.cpp.o"
+  "CMakeFiles/pkserver.dir/src/ssocket.cpp.o"
+  "pkserver"
+  "pkserver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pkserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
